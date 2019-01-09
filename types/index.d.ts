80dc67c3657d1ca5b8f50049ddcac2f17056a065
Roland Sadowski <szabla@gmail.com>
2019-01-09 12:29:39 +0000

    plugin &  js code: rename to InAppYapster

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,59 +10,59 @@ type channel = "loadstart" | "loadstop" | "loaderror" | "exit" | "message";
 
 interface Window {
     /**
-     * Opens a URL in a new InAppBrowser instance, the current browser instance, or the system browser.
+     * Opens a URL in a new InAppYapster instance, the current browser instance, or the system browser.
      * @param  url     The URL to load.
      * @param  target  The target in which to load the URL, an optional parameter that defaults to _self.
-     * @param  options Options for the InAppBrowser. Optional, defaulting to: location=yes.
+     * @param  options Options for the InAppYapster. Optional, defaulting to: location=yes.
      *                 The options string must not contain any blank space, and each feature's
      *                 name/value pairs must be separated by a comma. Feature names are case insensitive.
      */
-    open(url: string, target?: string, options?: string, replace?: boolean): InAppBrowser;
+    open(url: string, target?: string, options?: string, replace?: boolean): InAppYapster;
 }
 
 /**
  * The object returned from a call to window.open.
- * NOTE: The InAppBrowser window behaves like a standard web browser, and can't access Cordova APIs.
+ * NOTE: The InAppYapster window behaves like a standard web browser, and can't access Cordova APIs.
  */
-interface InAppBrowser extends Window {
+interface InAppYapster extends Window {
     onloadstart(type: Event): void;
-    onloadstop(type: InAppBrowserEvent): void;
-    onloaderror(type: InAppBrowserEvent): void;
-    onexit(type: InAppBrowserEvent): void;
+    onloadstop(type: InAppYapsterEvent): void;
+    onloaderror(type: InAppYapsterEvent): void;
+    onexit(type: InAppYapsterEvent): void;
     // addEventListener overloads
     /**
-     * Adds a listener for an event from the InAppBrowser.
-     * @param type      loadstart: event fires when the InAppBrowser starts to load a URL.
-     *                  loadstop: event fires when the InAppBrowser finishes loading a URL.
-     *                  loaderror: event fires when the InAppBrowser encounters an error when loading a URL.
-     *                  exit: event fires when the InAppBrowser window is closed.
+     * Adds a listener for an event from the InAppYapster.
+     * @param type      loadstart: event fires when the InAppYapster starts to load a URL.
+     *                  loadstop: event fires when the InAppYapster finishes loading a URL.
+     *                  loaderror: event fires when the InAppYapster encounters an error when loading a URL.
+     *                  exit: event fires when the InAppYapster window is closed.
      * @param callback  the function that executes when the event fires. The function is
-     *                  passed an InAppBrowserEvent object as a parameter.
+     *                  passed an InAppYapsterEvent object as a parameter.
      */
-    addEventListener(type: channel, callback: InAppBrowserEventListenerOrEventListenerObject): void;
+    addEventListener(type: channel, callback: InAppYapsterEventListenerOrEventListenerObject): void;
     // removeEventListener overloads
     /**
-     * Removes a listener for an event from the InAppBrowser.
+     * Removes a listener for an event from the InAppYapster.
      * @param type      The event to stop listening for.
-     *                  loadstart: event fires when the InAppBrowser starts to load a URL.
-     *                  loadstop: event fires when the InAppBrowser finishes loading a URL.
-     *                  loaderror: event fires when the InAppBrowser encounters an error when loading a URL.
-     *                  exit: event fires when the InAppBrowser window is closed.
+     *                  loadstart: event fires when the InAppYapster starts to load a URL.
+     *                  loadstop: event fires when the InAppYapster finishes loading a URL.
+     *                  loaderror: event fires when the InAppYapster encounters an error when loading a URL.
+     *                  exit: event fires when the InAppYapster window is closed.
      * @param callback  the function that executes when the event fires. The function is
-     *                  passed an InAppBrowserEvent object as a parameter.
+     *                  passed an InAppYapsterEvent object as a parameter.
      */
-    removeEventListener(type: channel, callback: InAppBrowserEventListenerOrEventListenerObject): void;
-    /** Closes the InAppBrowser window. */
+    removeEventListener(type: channel, callback: InAppYapsterEventListenerOrEventListenerObject): void;
+    /** Closes the InAppYapster window. */
     close(): void;
-    /** Hides the InAppBrowser window. Calling this has no effect if the InAppBrowser was already hidden. */
+    /** Hides the InAppYapster window. Calling this has no effect if the InAppYapster was already hidden. */
     hide(): void;
     /**
-     * Displays an InAppBrowser window that was opened hidden. Calling this has no effect
-     * if the InAppBrowser was already visible.
+     * Displays an InAppYapster window that was opened hidden. Calling this has no effect
+     * if the InAppYapster was already visible.
      */
     show(): void;
     /**
-     * Injects JavaScript code into the InAppBrowser window.
+     * Injects JavaScript code into the InAppYapster window.
      * @param script    Details of the script to run, specifying either a file or code key.
      * @param callback  The function that executes after the JavaScript code is injected.
      *                  If the injected script is of type code, the callback executes with
@@ -72,22 +72,22 @@ interface InAppBrowser extends Window {
      */
     executeScript(script: { code: string } | { file: string }, callback: (result: any) => void): void;
     /**
-     * Injects CSS into the InAppBrowser window.
+     * Injects CSS into the InAppYapster window.
      * @param css       Details of the script to run, specifying either a file or code key.
      * @param callback  The function that executes after the CSS is injected.
      */
     insertCSS(css: { code: string } | { file: string }, callback: () => void): void;
 }
 
-type InAppBrowserEventListenerOrEventListenerObject = InAppBrowserEventListener | InAppBrowserEventListenerObject;
+type InAppYapsterEventListenerOrEventListenerObject = InAppYapsterEventListener | InAppYapsterEventListenerObject;
 
-type InAppBrowserEventListener = (evt: InAppBrowserEvent) => void;
+type InAppYapsterEventListener = (evt: InAppYapsterEvent) => void;
 
-interface InAppBrowserEventListenerObject {
-    handleEvent(evt: InAppBrowserEvent): void;
+interface InAppYapsterEventListenerObject {
+    handleEvent(evt: InAppYapsterEvent): void;
 }
 
-interface InAppBrowserEvent extends Event {
+interface InAppYapsterEvent extends Event {
     /** the eventname, either loadstart, loadstop, loaderror, or exit. */
     type: string;
     /** the URL that was loaded. */
@@ -99,5 +99,5 @@ interface InAppBrowserEvent extends Event {
 }
 
 interface Cordova {
-    InAppBrowser: InAppBrowser;
+    InAppYapster: InAppYapster;
 }
