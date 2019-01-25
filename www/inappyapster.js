bae3310ac4101567ac3236eeba338cc4f9206bac
Roland Sadowski <szabla@gmail.com>
2019-01-25 19:34:36 +0000

    move init boilerplate into the plugin

--- www/inappyapster.js
+++ www/inappyapster.js
@@ -44,6 +44,7 @@
     }
 
     InAppYapster.prototype = {
+
         _eventHandler: function (event) {
             if (event && (event.type in this.channels)) {
                 if (event.type === 'beforeload') {
@@ -95,29 +96,67 @@
             } else {
                 throw new Error('insertCSS requires exactly one of code or file to be specified');
             }
-        }
+        },
+        
+        initialize: function (opts) {
+            var yapsterBaseUrl = opts.baseUrl;
+            var beforeloadCB = function(params, callback) {
+                console.log("_beforeloadCallback");
+                console.log("_beforeloadCallback: " + yapsterBaseUrl);
+                if (params.url.startsWith("file:///") ||
+                    params.url.startsWith(yapsterBaseUrl)) {
+                    // Load this URL in the inAppYapster.
+                    callback(params.url);
+                } else if (params.url.startsWith("itms") ||
+                           params.url.startsWith("https://itunes.apple.com") ||
+                           params.url.startsWith("https://play.google.com/store/apps/details")) {
+                    var otherOpts = {baseUrl: params.url};
+                    cordova.InAppBrowser.open(params.url, "_system", "")
+                }  else {
+                    // The callback is not invoked, so the page will not be loaded.
+                    alert("Can't open the url from this Yapster version. Please download the full Yapster app.");
+                }
+            }
+
+            this.addEventListener('beforeload', beforeloadCB);
+        },
     };
 
-    module.exports = function (strUrl, strWindowName, strWindowFeatures, callbacks) {
+    module.exports = function (opts, strWindowName, strWindowFeatures, callbacks) {
         // Don't catch calls that write to existing frames (e.g. named iframes).
         if (window.frames && window.frames[strWindowName]) {
             var origOpenFunc = modulemapper.getOriginalSymbol(window, 'open');
             return origOpenFunc.apply(window, arguments);
         }
 
-        strUrl = urlutil.makeAbsolute(strUrl);
-        var iab = new InAppYapster();
+        var strUrl = null;
+        console.log("opening yapster");
+        var yapsterBaseUrl = opts.baseUrl;
+        console.log("baseUrl: " + yapsterBaseUrl);
 
+        var platform = "unknown";
+        if (cordova && cordova.platformId) {
+            platform = cordova.platformId;
+        }
+        let host = opts.baseUrl;
+        let authToken = opts.authToken;
+        let fullUrl = `${host}?lite&api-key=${authToken}&platform=${platform}`;
+        let options = "hidden=yes,location=no,beforeload=yes,hidenavigationbuttons=yes,disallowoverscroll=yes";
+        
+        strUrl = urlutil.makeAbsolute(fullUrl);
+        var iab = new InAppYapster();
+        iab.initialize(opts);
+        console.log("strUrl " + strUrl);
+        
         callbacks = callbacks || {};
         for (var callbackName in callbacks) {
             iab.addEventListener(callbackName, callbacks[callbackName]);
         }
-
         var cb = function (eventname) {
             iab._eventHandler(eventname);
         };
 
-        strWindowFeatures = strWindowFeatures || '';
+        strWindowFeatures = strWindowFeatures || options;
 
         exec(cb, cb, 'InAppYapster', 'open', [strUrl, strWindowName, strWindowFeatures]);
         return iab;
